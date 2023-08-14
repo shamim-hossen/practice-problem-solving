2e169d53cb36d5681853101e657808c9e3b41222
Shamim Hossen <shamimrejone@gmail.com>
2023-08-14 23:42:50 +0600

    revise loop

---
+++ basic/cp1.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    /*======explorer more==========
+    1.operator >,<, & , 
+    2.increment, decrmentOperator effect when store or assign
+    3.localScope, globalScope, priority 
+    
+    
+    */
+
+
+//loop=> while 
+    // int i = 1;
+    // while (i<=11)
+    // {
+    //     printf("%d\n",i);
+    //     i++;
+    // }
+
+
+//loop=> do while
+    // int i=1;
+    // do{
+    //     printf("Count = %d\n",i);
+    //     i++;
+    // }while(i<=5);
+
+
+// loop=> for
+    // for (int i=1;i<=5;i++){
+    //     printf("%d\n",i);
+    // }
+
+
+/*=========Time complexity=========
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    return 0;
+}
