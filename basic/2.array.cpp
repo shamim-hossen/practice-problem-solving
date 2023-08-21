fbb964a76a3859a3749ecc1b2f68d8cb50070fca
Shamim Hossen <shamimrejone@gmail.com>
2023-08-21 23:52:15 +0600

    add cmnt

--- basic/2.array.cpp
+++ basic/2.array.cpp
@@ -1,17 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    /*======Data structure=> specify========
+        1.Organization of data(data store kase kartehe)
+            arrange data
+        2.Accessing methods(data element ko access kase kare) 
+            Array==base address
+            linklist==address of first node address
+            tree==root
+        3.Degree of association(liner,nonliner)age koita element pore koita element
+            linier==>array,age ak pore ak element
+            nonlinier==>tree,graph,ak or akse jada hosakta he
+        4.Processing method(rule & regulation)
+            binary search==> left small right big
+
+    nonpremitive data type=>jiska definition batana pare
+    premitive data type=>(jiska definition batana na pare, compiler of programming laguage ko )
     
+    Homogeneous data==array, same time data
+    Heterogeneous data==tree ka node,left/right child pointer inside data
+
+
+
+    */
     int a[10];
     //input
     for(int i=0;i<3;i++){
         scanf("%d",&a[i]);
     }
-    
     //output
     for(int i=0;i<3;i++){
         printf("%d",a[i]);
     }
+
+    //2d array
 
     return 0;
 }
