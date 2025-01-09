0abcd1c484f773315e50a0599a86602e891cb25b
shamim-hossen <shamimrejone@gmail.com>
2025-01-10 00:00:12 +0600

    other way

--- random/ageInDays.cpp
+++ random/ageInDays.cpp
@@ -14,3 +14,25 @@ int main(){
     cout << d << " dia(s)" << endl;
     return 0;
 }
+
+/**
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, years, months, days;
+    cin >> n;
+
+    years = n / 365;
+    n %= 365;  // Update 'n' to the remaining days after calculating years
+
+    months = n / 30;
+    days = n % 30;  // Remaining days after calculating months
+
+    cout << years << " ano(s)" << endl;
+    cout << months << " mes(es)" << endl;
+    cout << days << " dia(s)" << endl;
+
+    return 0;
+}
+ */
