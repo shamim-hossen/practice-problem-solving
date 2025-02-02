9d840412160d32390606e7b85fd2f9a5cba390b8
shamim-hossen <shamimrejone@gmail.com>
2025-02-02 22:50:32 +0600

    Multiplication Table

---
+++ random/multiplicationTable.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    // Print the first 10 multiples of N
+    for (int i = 1; i <= 10; ++i) {
+        cout << N * i << endl;
+    }
+
+    return 0;
+}
