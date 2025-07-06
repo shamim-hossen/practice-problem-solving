4dfda55b5bea0622c6a687e14ce4ec0be5e9a570
shamim-hossen <shamimrejone@gmail.com>
2025-07-07 03:23:19 +0600

    print even odd separately

--- loop/basic.c
+++ loop/basic.c
@@ -30,3 +30,34 @@ int main() {
 
     return 0;
 }
+
+
+
+
+/**
+1.print even and odd number separately 
+
+**/
+#include <stdio.h>
+
+int main() {
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+
+    printf("Odd numbers:\n");
+    for (int i = n1; i <= n2; i++) {
+        if (i % 2 != 0) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\nEven numbers:\n");
+    for (int i = n1; i <= n2; i++) {
+        if (i % 2 == 0) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
+
