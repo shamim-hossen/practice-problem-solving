9b17d06d54f0d0aadbde30ea456ea179a1e87523
shamim-hossen <shamimrejone@gmail.com>
2025-07-06 23:57:01 +0600

    print odd num

---
+++ loop/basic.c
@@ -0,0 +1,32 @@
+/**
+1.Print odd number from n1 to n2
+
+2. One linear condition 
+for (int i = (n1 % 2 == 0 ? n1 + 1 : n1); i <= n2; i += 2) {
+        printf("%d\n", i);
+    }
+
+3.using continue (less effective)
+for (int i = n1; i <= n2; i++) {
+        if (i % 2 == 0) continue;
+        printf("%d\n", i);
+    }
+**/
+
+#include <stdio.h>
+
+int main() {
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+
+    // Make sure n1 is odd
+    if (n1 % 2 == 0) {
+        n1++;
+    }
+
+    for (int i = n1; i <= n2; i += 2) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
