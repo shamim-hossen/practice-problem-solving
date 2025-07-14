50bead48606d4ccdda9f7b18b7664874fc3a170a
shamim-hossen <shamimrejone@gmail.com>
2025-07-14 18:21:23 +0600

    palindrome num

--- loop/basic.c
+++ loop/basic.c
@@ -111,3 +111,26 @@ int main() {
 
     return 0;
 }
+
+
+// palindrome number
+#include<stdio.h>
+int main (){
+    int num;
+    scanf("%d",&num);
+    int x=num, rev=0;
+    if(num < 0){
+        return 0;
+    }
+    while(num!=0){
+        rev=rev*10+num%10;
+        num=num/10;
+    }
+    if (x==rev){
+        return 1;
+    }
+
+    return 0;
+}
+
+
