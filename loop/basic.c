0126f2d227081c027d6bc6a77f8d6e7f42029b1b
shamim-hossen <shamimrejone@gmail.com>
2025-07-17 19:04:00 +0600

    palindrome another way

--- loop/basic.c
+++ loop/basic.c
@@ -133,4 +133,26 @@ int main (){
     return 0;
 }
 
+// palindrome other way
+#include <stdio.h>
+
+int main() {
+    int num, reversed = 0, original, remainder;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+
+    if (original == reversed)
+        printf("%d is a palindrome.\n", original);
+    else
+        printf("%d is not a palindrome.\n", original);
+
+    return 0;
+}
 
