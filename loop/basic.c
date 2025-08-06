829074614924a8792c6ae7178cbf21b5ded53c72
shamim-hossen <shamimrejone@gmail.com>
2025-08-06 23:41:39 +0600

    leap year

--- loop/basic.c
+++ loop/basic.c
@@ -412,3 +412,21 @@ int main() {
 
     return 0;
 }
+
+
+/*
+    Check if a Year is a Leap Year
+*/
+#include <stdio.h>
+int main() {
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        printf("Leap year\n");
+    else
+        printf("Not a leap year\n");
+
+    return 0;
+}
