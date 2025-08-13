32811ea4faa159e39140bddb797a8e11da0b277f
shamim-hossen <shamimrejone@gmail.com>
2025-08-13 23:34:41 +0600

    find prime num

--- loop/basic.c
+++ loop/basic.c
@@ -511,3 +511,32 @@ int main() {
     printf("Reversed number: %d\n", reverse);
     return 0;
 }
+
+
+/**
+ * Check if a Number is Prime
+ */
+#include <stdio.h>
+int main() {
+    int n, i, isPrime = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(n <= 1) {
+        isPrime = 0;
+    } else {
+        for(i = 2; i <= n / 2; i++) {
+            if(n % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if(isPrime)
+        printf("Prime\n");
+    else
+        printf("Not Prime\n");
+
+    return 0;
+}
