571f57c2e243a35ad414c1c5a542a4238c21cfd1
shamim-hossen <shamimrejone@gmail.com>
2025-08-18 17:56:20 +0600

    LCM

--- loop/basic.c
+++ loop/basic.c
@@ -560,3 +560,25 @@ int main() {
     printf("GCD = %d\n", a);
     return 0;
 }
+
+/**
+ * Find LCM (Least Common Multiple)
+ */
+#include <stdio.h>
+int main() {
+    int a, b, max;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    max = (a > b) ? a : b;
+
+    while(1) {
+        if(max % a == 0 && max % b == 0) {
+            printf("LCM = %d\n", max);
+            break;
+        }
+        max++;
+    }
+
+    return 0;
+}
