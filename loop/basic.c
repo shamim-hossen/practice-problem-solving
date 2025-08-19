87292e97f925bee1571c1afea4d38f4cdda6f0da
shamim-hossen <shamimrejone@gmail.com>
2025-08-19 18:13:02 +0600

    armstrong num

--- loop/basic.c
+++ loop/basic.c
@@ -582,3 +582,27 @@ int main() {
 
     return 0;
 }
+
+/**
+ * Check Armstrong Number
+ */
+#include <stdio.h>
+int main() {
+    int num, original, sum = 0, digit;
+    printf("Enter a 3-digit number: ");
+    scanf("%d", &num);
+
+    original = num;
+    while(num != 0) {
+        digit = num % 10;
+        sum += digit * digit * digit;
+        num /= 10;
+    }
+
+    if(sum == original)
+        printf("Armstrong Number\n");
+    else
+        printf("Not Armstrong\n");
+
+    return 0;
+}
