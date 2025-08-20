7922b6198e769fa661e642bc14dcfbf1bce44b6c
shamim-hossen <shamimrejone@gmail.com>
2025-08-20 18:32:30 +0600

    multiplication table

--- loop/basic.c
+++ loop/basic.c
@@ -606,3 +606,19 @@ int main() {
 
     return 0;
 }
+
+/**
+ * Print Multiplication Table
+ */
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+
+    return 0;
+}
