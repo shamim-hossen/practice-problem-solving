90574ed0f3ea4f19effd13238961c025919edaa4
shamim-hossen <shamimrejone@gmail.com>
2025-08-22 23:48:29 +0600

    largest digit in num

--- loop/basic.c
+++ loop/basic.c
@@ -644,3 +644,23 @@ int main() {
 
     return 0;
 }
+
+/**
+ * Find the Largest Digit in a Number
+ */
+#include <stdio.h>
+int main() {
+    int num, max = 0, digit;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while(num != 0) {
+        digit = num % 10;
+        if(digit > max)
+            max = digit;
+        num /= 10;
+    }
+
+    printf("Largest digit = %d\n", max);
+    return 0;
+}
