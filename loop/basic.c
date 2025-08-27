f3f0054fbfe08c7926d2de6166768f46bd33c97f
shamim-hossen <shamimrejone@gmail.com>
2025-08-27 23:53:29 +0600

    product of digit

--- loop/basic.c
+++ loop/basic.c
@@ -751,3 +751,22 @@ int main() {
 
     return 0;
 }
+
+
+/**
+ * Find Product of Digits
+ */
+#include <stdio.h>
+int main() {
+    int num, product = 1;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while(num != 0) {
+        product *= num % 10;
+        num /= 10;
+    }
+
+    printf("Product of digits = %d\n", product);
+    return 0;
+}
