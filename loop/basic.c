fa5246986cd1f8c988258bfa2463dc1265212c14
shamim-hossen <shamimrejone@gmail.com>
2025-09-02 23:14:15 +0600

    all divisor of a num

--- loop/basic.c
+++ loop/basic.c
@@ -887,3 +887,21 @@ int main() {
 
     return 0;
 }
+
+/**
+ * Print All Divisors of a Number
+ */
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Divisors: ");
+    for(int i = 1; i <= num; i++) {
+        if(num % i == 0)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
