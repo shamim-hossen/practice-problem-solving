2174cf3799989dcaccfc6f119fd8b79c1391b5ee
shamim-hossen <shamimrejone@gmail.com>
2025-09-18 23:17:43 +0600

    binary to decimal

--- loop/basic.c
+++ loop/basic.c
@@ -1165,3 +1165,24 @@ int main() {
 
     return 0;
 }
+
+/**
+ * Convert Binary Number to Decimal
+ */
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int binary, decimal = 0, i = 0, digit;
+    printf("Enter binary number: ");
+    scanf("%d", &binary);
+
+    while(binary > 0) {
+        digit = binary % 10;
+        decimal += digit * pow(2, i);
+        binary /= 10;
+        i++;
+    }
+
+    printf("Decimal = %d\n", decimal);
+    return 0;
+}
