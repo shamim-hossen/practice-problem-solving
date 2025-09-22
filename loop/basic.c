aceca8c89bb4af07140a5629062824fcda8c9976
shamim-hossen <shamimrejone@gmail.com>
2025-09-22 22:23:14 +0600

    max and min in arr

--- loop/basic.c
+++ loop/basic.c
@@ -1254,3 +1254,30 @@ int main() {
     printf("Sum of array elements = %d\n", sum);
     return 0;
 }
+
+
+/**
+ * Find Maximum and Minimum in array
+ */
+#include <stdio.h>
+int main() {
+    int arr[100], n, max, min;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    max = min = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+        if(arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Maximum = %d\n", max);
+    printf("Minimum = %d\n", min);
+    return 0;
+}
