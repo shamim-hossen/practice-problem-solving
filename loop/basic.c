b78bccc34435edfc7747f87626f4b6c2e8fe52a5
shamim-hossen <shamimrejone@gmail.com>
2025-09-23 23:32:53 +0600

    reverse an arr

--- loop/basic.c
+++ loop/basic.c
@@ -1281,3 +1281,26 @@ int main() {
     printf("Minimum = %d\n", min);
     return 0;
 }
+
+/**
+ * Reverse an Array
+ */
+#include <stdio.h>
+
+int main() {
+    int arr[100], n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Reversed array: ");
+    for(int i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
