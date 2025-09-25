819e9576019cdbae6c45390f4932f8f60e9a10ea
shamim-hossen <shamimrejone@gmail.com>
2025-09-25 23:48:40 +0600

    search for an element

--- loop/basic.c
+++ loop/basic.c
@@ -1304,3 +1304,32 @@ int main() {
 
     return 0;
 }
+
+/**
+ * Search for an Element
+ */
+#include <stdio.h>
+int main() {
+    int arr[100], n, key, found = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            printf("Element found at index %d (position %d)\n", i, i + 1);
+            found = 1;
+            break;
+        }
+    }
+    if(!found) {
+        printf("Element not found.\n");
+    }
+
+    return 0;
+}
