1454d2390c84475fe72fffbdd7194c399301da6a
shamim-hossen <shamimrejone@gmail.com>
2025-09-26 23:38:00 +0600

    count even and odd element

--- loop/basic.c
+++ loop/basic.c
@@ -1333,3 +1333,28 @@ int main() {
 
     return 0;
 }
+
+/**
+ * Count Even and Odd Elements
+ */
+#include <stdio.h>
+int main() {
+    int arr[100], n, even = 0, odd = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even elements: %d\n", even);
+    printf("Odd elements: %d\n", odd);
+
+    return 0;
+}
