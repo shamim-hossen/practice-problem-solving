57d6e7ca62939a0203a340410c0b65d44c323082
shamim-hossen <shamimrejone@gmail.com>
2025-09-29 17:57:52 +0600

    right-angle triangle rotated

--- loop/basic.c
+++ loop/basic.c
@@ -1392,3 +1392,28 @@ int main() {
     }
     return 0;
 }
+
+/**
+ * Right-angled triangle (rotated 180°)
+ */
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    // Print the rotated right-angled triangle
+    for (int i = 1; i <= n; i++) {
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            printf("  ");
+        }
+        // Print stars
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        // Move to the next line
+        printf("\n");
+    }
+    return 0;
+}
