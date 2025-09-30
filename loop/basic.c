99118df3b76be073ae7e0f3bf1e6868a0d81cf73
shamim-hossen <shamimrejone@gmail.com>
2025-09-30 17:44:08 +0600

    inverted right-angled triangle

--- loop/basic.c
+++ loop/basic.c
@@ -1417,3 +1417,22 @@ int main() {
     }
     return 0;
 }
+
+
+/**
+ * Inverted Right-Angled Triangle
+ */
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    // Print inverted right-angled triangle
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
