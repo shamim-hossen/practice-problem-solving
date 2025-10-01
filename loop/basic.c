8d0f66df77c62f5aa6132c855518a278570c6aad
shamim-hossen <shamimrejone@gmail.com>
2025-10-01 18:01:00 +0600

    pyramid pattern of *

--- loop/basic.c
+++ loop/basic.c
@@ -1436,3 +1436,28 @@ int main() {
     }
     return 0;
 }
+
+
+/**
+ * Pyramid pattern of `*`
+ */
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    // Pyramid pattern
+    for (int i = 1; i <= n; i++) {
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        // Print stars
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        // Move to the next line
+        printf("\n");
+    }
+    return 0;
+}
