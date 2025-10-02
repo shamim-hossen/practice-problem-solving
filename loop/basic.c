50908b9c93c7c93a3fa85198bed7b737f0a867f4
shamim-hossen <shamimrejone@gmail.com>
2025-10-02 18:58:03 +0600

    inverted pyramid pattern

--- loop/basic.c
+++ loop/basic.c
@@ -1461,3 +1461,26 @@ int main() {
     }
     return 0;
 }
+
+/**
+ * Inverted Pyramid Pattern of '*'
+ */
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    // Inverted pyramid logic
+    for (int i = n; i >= 1; i--) {
+        // Print leading spaces
+        for (int j = 1; j <= n - i; j++)
+            printf(" ");
+        // Print stars
+        for (int j = 1; j <= 2 * i - 1; j++)
+            printf("*");
+        // Move to the next line
+        printf("\n");
+    }
+    return 0;
+}
