37216be30367ace2824c046d176ce4f39210da09
shamim-hossen <shamimrejone@gmail.com>
2025-10-03 23:05:07 +0600

    half pyramid using num

--- loop/basic.c
+++ loop/basic.c
@@ -1484,3 +1484,25 @@ int main() {
     }
     return 0;
 }
+
+/**
+ * Half pyramid using numbers
+ */
+#include <stdio.h>
+int main() {
+    int n;
+    // Input number of rows
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    // Outer loop for rows
+    for (int i = 1; i <= n; i++) {
+        // Inner loop for numbers in each row
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        // Move to next line after each row
+        printf("\n");
+    }
+    return 0;
+}
