9aa7dbdecb59c2819a2eccfce0bb8862580317c5
shamim-hossen <shamimrejone@gmail.com>
2025-10-14 18:29:44 +0600

    hollow square pattern

--- loop/basic.c
+++ loop/basic.c
@@ -1556,3 +1556,24 @@ int main() {
 }
 
 
+/**
+ * Hollow Square Pattern
+ */
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            // Print * at border positions, spaces inside
+            if (i == 1 || i == n || j == 1 || j == n)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
