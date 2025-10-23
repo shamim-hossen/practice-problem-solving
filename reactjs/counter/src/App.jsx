49991d422a75cacbc81b00c45afd6b343ef8bae6
shamim-hossen <shamimrejone@gmail.com>
2025-10-23 23:29:40 +0600

    increment and decrement functionality

--- reactjs/counter/src/App.jsx
+++ reactjs/counter/src/App.jsx
@@ -4,15 +4,17 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(1);
+    const [count, setCount] = useState(0);
 
     return (
         <>
-            <h1>Counter</h1>
+            <h1>Counter {count} </h1>
 
-            <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
+            <button onClick={() => setCount((count) => count - 1)}>-</button>
+            <button onClick={() => setCount((count) => (count = 0))}>
+                reset
             </button>
+            <button onClick={() => setCount((count) => count + 1)}>+</button>
         </>
     );
 }
