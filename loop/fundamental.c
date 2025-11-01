52e20c6240ef4bb634bc5853472d8086aab7cff0
shamim-hossen <shamimrejone@gmail.com>
2025-11-01 23:56:33 +0600

    basic inp out

---
+++ loop/fundamental.c
@@ -0,0 +1,14 @@
+/**
+Basic Input/Output & Data Types
+
+1.Print your name, age, and country.
+2.Add, subtract, multiply, divide two numbers.
+3.Find the area and perimeter of a rectangle.
+4.Find the area and circumference of a circle.
+5.Convert Celsius ↔ Fahrenheit.
+6.Calculate simple interest and compound interest.
+7.Convert days → years, weeks, and days.
+8.Swap two numbers (using temp and without temp).
+9.Find ASCII value of a character.
+10.Compute the average of three numbers.
+**/
