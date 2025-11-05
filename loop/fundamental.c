9345af930943b9b725c597d82f82bbb9042797ff
shamim-hossen <shamimrejone@gmail.com>
2025-11-05 19:30:38 +0600

    loop list

--- loop/fundamental.c
+++ loop/fundamental.c
@@ -29,3 +29,25 @@ Conditional Statements (if, else, switch)
 9.Check divisibility (by 3 and 5, etc.).
 10.Check triangle type (equilateral, isosceles, scalene).
  */
+
+
+
+/**
+Loops (for, while, do-while)
+
+1.Print numbers from 1 to N.
+2.Print even/odd numbers between 1 to N.
+3.Sum of first N natural numbers.
+4.Factorial of a number.
+5.Reverse a number.
+6.Count digits of a number.
+7.Sum of digits of a number.
+8.Check palindrome number.
+9.Check Armstrong number.
+10.Print multiplication table.
+11.Find power (x^y).
+12.GCD and LCM of two numbers.
+13.Fibonacci series up to N terms.
+14.Prime number check.
+15.Display all prime numbers between 1–N.
+ */
