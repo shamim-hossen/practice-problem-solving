ab170be75cd5efc6b8a98b99d6603e21d636faac
shamim-hossen <shamimrejone@gmail.com>
2025-12-05 23:43:09 +0600

    improved counter code

--- reactjs/counter/src/App.jsx
+++ reactjs/counter/src/App.jsx
@@ -8,29 +8,30 @@ function App() {
     const [show, setShow] = useState(true);
 
     const increment = () => {
-        setCount((prev) => (prev < 5 ? prev + 1 : prev));
+        setCount((prev) => (prev < 5 ? prev + 1 : prev)); //if (count < 5) setCount(count + 1);
     };
 
     const decrement = () => {
-        setCount((prev) => (prev > -5 ? prev - 1 : prev));
+        setCount((prev) => (prev > -5 ? prev - 1 : prev)); //if (count > -5) setCount(count - 1);
     };
 
     // const reset = () => {
-    //     setCount(0);
+    //     setCount(0);// const reset = () => setCount(0);
     // };
 
     const handleShow = () => {
-        setShow((prev) => !prev);
+        setShow((prev) => !prev); //const toggleShow = () => setShow(!show);
     };
 
     return (
         <>
-            <h1>Counter {count} </h1>
+            <h1>Counter : {count} </h1>
             <button onClick={decrement}>-</button>
             <button onClick={() => setCount((count) => (count = 0))}>
-                reset
+                reset {/*onClick={() => setCount(0)} */}
             </button>
             <button onClick={increment}>+</button>
+
             <h1>Toggle text</h1>
             {show && "This is show text"}
             <button onClick={handleShow}>{show ? "Hide" : "Show"}</button>
