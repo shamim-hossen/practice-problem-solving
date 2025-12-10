b3d966de8d965b509cdb4aca7be51b759b052989
shamim-hossen <shamimrejone@gmail.com>
2025-12-10 19:45:44 +0600

    find str length

--- loop/basic.c
+++ loop/basic.c
@@ -1644,3 +1644,25 @@ int main(){
     printf("You entered: %s", str);
     return 0;
 }
+
+
+
+
+
+/**
+ * Find String Length (without strlen)
+ */
+#include <stdio.h>
+int main() {
+    char str[100];
+    int length = 0;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    for (int i = 0; str[i] != '\0'; i++) {
+        length++;
+    }
+    // Remove newline from fgets count
+    if (str[length - 1] == '\n') length--;
+    printf("Length = %d\n", length);
+    return 0;
+}
