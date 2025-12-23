021d84d83d5c70480875bd7b19dbfe808aa05873
shamim-hossen <shamimrejone@gmail.com>
2025-12-24 01:44:16 +0600

    count vowel,consonant, digit,space

--- loop/basic.c
+++ loop/basic.c
@@ -1748,3 +1748,31 @@ int main() {
     printf("Copied string: %s\n", dest);
     return 0;
 }
+
+/**
+ * Count Vowels, Consonants, Digits, Spaces
+ */
+#include <stdio.h>
+#include <ctype.h>
+int main() {
+    char str[200];
+    int vowels = 0, consonants = 0, digits = 0, spaces = 0;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = tolower(str[i]);
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+                vowels++;
+            else
+                consonants++;
+        }
+        else if (ch >= '0' && ch <= '9')
+            digits++;
+        else if (ch == ' ')
+            spaces++;
+    }
+    printf("Vowels: %d\nConsonants: %d\nDigits: %d\nSpaces: %d\n",
+        vowels, consonants, digits, spaces);
+    return 0;
+}
