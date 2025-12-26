199bac8abcee6179fba509bc04ae8dccfec1b626
shamim-hossen <shamimrejone@gmail.com>
2025-12-26 20:06:11 +0600

    convert str to uppercase and lowercase

--- loop/basic.c
+++ loop/basic.c
@@ -1776,3 +1776,23 @@ int main() {
         vowels, consonants, digits, spaces);
     return 0;
 }
+
+
+/**
+ * Convert String to Uppercase & Lowercase
+ */
+#include <stdio.h>
+#include <ctype.h>
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    printf("Uppercase: ");
+    for (int i = 0; str[i] != '\0'; i++)
+        printf("%c", toupper(str[i]));
+    printf("\nLowercase: ");
+    for (int i = 0; str[i] != '\0'; i++)
+        printf("%c", tolower(str[i]));
+    printf("\n");
+    return 0;
+}
