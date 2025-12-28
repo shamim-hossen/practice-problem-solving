f6793afe4a3f77b7b612c710d71db1f849fd01b8
shamim-hossen <shamimrejone@gmail.com>
2025-12-28 20:59:27 +0600

    concatenate two str without strcat

--- loop/basic.c
+++ loop/basic.c
@@ -1796,3 +1796,25 @@ int main() {
     printf("\n");
     return 0;
 }
+
+/**
+ * Concatenate Two Strings (without strcat)
+ */
+#include <stdio.h>
+int main() {
+    char s1[200], s2[100];
+    int i = 0, j = 0;
+    printf("Enter first string: ");
+    fgets(s1, sizeof(s1), stdin);
+    printf("Enter second string: ");
+    fgets(s2, sizeof(s2), stdin);
+
+    while (s1[i] != '\0') i++;
+    if (s1[i - 1] == '\n') i--; 
+    while (s2[j] != '\0') {
+        s1[i++] = s2[j++];
+    }
+    s1[i] = '\0';
+    printf("Concatenated: %s\n", s1);
+    return 0;
+}
