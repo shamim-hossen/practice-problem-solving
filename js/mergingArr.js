9d6eb7b69fe2edb956fc73775f31e318f8162b46
shamim-hossen <shamimrejone@gmail.com>
2026-01-05 19:32:40 +0600

    merging arr using spread operator

---
+++ js/mergingArr.js
@@ -0,0 +1,13 @@
+// Using the spread operator
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const mergedArray = [...array1, ...array2];
+console.log(mergedArray); // Output: [1, 2, 3, 4, 5, 6]
+
+
+
+
+
+
+
+
