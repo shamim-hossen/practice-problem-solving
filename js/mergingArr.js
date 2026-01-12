5f58d82ca9833495c82cf060710c84f170301e87
shamim-hossen <shamimrejone@gmail.com>
2026-01-12 23:21:13 +0600

    marged arr using push

--- js/mergingArr.js
+++ js/mergingArr.js
@@ -11,7 +11,10 @@ const b = [3, 4];
 const merged = a.concat(b);
 console.log(merged);
 
-
-
+// push() with the Spread Operator
+const arr1 = [1, 2];
+const arr2 = [3, 4];
+arr1.push(...arr2); 
+console.log(arr1); // [1, 2, 3, 4]
 
 
